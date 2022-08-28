7d836ff0b7fc46208b802ff33f0fbb44fa73ea63
Gabriel Marques Frahm <gabriel_frahm@hotmail.com>
2022-08-28 11:44:28 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
